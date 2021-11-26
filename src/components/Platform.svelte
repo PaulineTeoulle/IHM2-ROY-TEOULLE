567229f9b946eda1d06d27ce164524435b7a5482
Pauline <pa.teoulle@gmail.com>
2021-11-26 16:18:06 +0100

    Add Banner Profile

---
+++ src/components/Platform.svelte
@@ -0,0 +1,20 @@
+<script>
+	
+
+</script>
+
+
+<div class="Platform">
+ Platform
+</div>
+
+<style lang="scss">
+	@import '../styles/color.scss';
+
+    .Platform{
+        background-color: green;
+        width: 100%;
+        margin: 0 24px;
+    }
+	
+</style>
--- src/views/profil.svelte
+++ src/views/profil.svelte
@@ -1,3 +1,24 @@
-<h1>
-    PROFIL
-</h1>
+<script>
+    import Banner from "../components/Banner.svelte";
+	import Post from "../components/Post.svelte";
+    import Platform from "../components/Platform.svelte";
+    let posts = [
+		{"user" : "Sxyrp", "thumbnail": "kiwi.jpg", "game" : "Dofus", "message" : "Quête tutu presque terminée !!! Demain gros rush temporis (bougle dj gelé pour drop le gelano) :p", "image" : "tutu.png"},
+		{"user" : "Bamboo", "thumbnail": "scratch.jpg", "game" : "Dofus", "message" : "Caverne des Bworks", "image" : "caverne.png"}
+	]
+</script>
+
+<div class="Profil">
+    <div>
+        <Banner/>
+        {#each posts as post}
+         <Post post={post}/>
+         {/each}
+    </div>
+  <div><Platform/></div>
+  
+</div>
+
+<style lang="scss">
+    @import '../styles/profil.scss';
+</style>
