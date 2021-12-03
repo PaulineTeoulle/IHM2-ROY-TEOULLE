57ecea657ece8fa35009895e4588579dc4e40118
Pauline <pa.teoulle@gmail.com>
2021-12-03 13:46:41 +0100

    Fix CSS for profile (proportions)

--- src/views/profil.svelte
+++ src/views/profil.svelte
@@ -1,23 +1,44 @@
 <script>
     import Banner from "../components/Banner.svelte";
-	import Post from "../components/Post.svelte";
+    import Post from "../components/Post.svelte";
     import Platform from "../components/Platform.svelte";
-    let posts = [
-        {"user" : "Sxyrp", "thumbnail": "kiwi.jpg", "game" : "Dofus", "message" : "Quête tutu presque terminée !!! Demain gros rush temporis (bougle dj gelé pour drop le gelano) :p", "image" : "tutu.png", "comments" : "13", "likes" : 70, "date" : "13 nov. 2021"},
-		{"user" : "Sxyrp", "thumbnail": "kiwi.jpg", "game" : "Dofus", "message" : "Super le donjon avec les potes", "image" : "caverne.png" ,"comments" : "8", "likes" : 53, "date" : "12 nov. 2021"}
-	]
-</script>
 
-<div class="Profil">
-    <div>
-        <Banner/>
-        {#each posts as post}
-         <Post post={post}/>
-        {/each}
+    let posts = [
+        {
+            "user": "Sxyrp",
+            "thumbnail": "kiwi.jpg",
+            "game": "Dofus",
+            "message": "Quête tutu presque terminée !!! Demain gros rush temporis (bougle dj gelé pour drop le gelano) :p",
+            "image": "tutu.png",
+            "comments": "13",
+            "likes": 70,
+            "date": "13 nov. 2021"
+        },
+        {
+            "user": "Sxyrp",
+            "thumbnail": "kiwi.jpg",
+            "game": "Dofus",
+            "message": "Super le donjon avec les potes",
+            "image": "caverne.png",
+            "comments": "8",
+            "likes": 53,
+            "date": "12 nov. 2021"
+        }
+    ]
+</script>
+<main>
+    <div id="Profil">
+        <div class="MidContainer">
+            <Banner/>
+            {#each posts as post}
+                <Post post={post}/>
+            {/each}
+        </div>
+        <div id="RigthBar"><Platform/></div >
     </div>
-  <div><Platform/></div>
-</div>
+</main>
 
 <style lang="scss">
-    @import '../styles/profil.scss';
+  @import '../styles/profil.scss';
+  @import '../styles/rightbar.scss';
 </style>
